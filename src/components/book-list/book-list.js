bb14e672f21e1a8717dd287c4a6d2da58145d103
Alexandra Shestakova <shestakova18@bk.ru>
2020-10-07 14:13:40 +0300

    added promise to service

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
+import Spinner from '../spinner';
 import { withBookstoreService } from '../hoc';
 import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
@@ -10,14 +11,19 @@ import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks().then((data) => {
+      booksLoaded(data);
+    });
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
     return (
       <ul className="book-list">
         {books.map((book) => {
@@ -32,8 +38,8 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
 };
 
 const mapDispatchToProps = {
